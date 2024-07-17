1a6d5cbf62a3afd9de79e2b2b829d02d76030ba4
Arturo <ablrcomx@gmail.com>
2024-07-17 14:14:43 -0700

    Text-centering heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function PromptGenerator() {
       <div className="w-full">
         <header className="h-[150px] mb-6 flex flex-col justify-center items-center">
           <Image src="/logo_crespf.jpg" alt={"Logotipo CRESPF"} width={100} height={100} className="my-3" />
-          <h1 className="uppercase font-bold">generador de prompts educativos del CRESPF</h1>  
+          <h1 className="text-center uppercase font-bold">generador de prompts educativos del CRESPF</h1>  
         </header>
         {/* <PromptGeneratorHeadingImage className="block w-full h-auto" /> */}
         {/* <Image className="object-cover" src={promptGeneratorHeadingImage} alt="Imagen de encabezado del generador de prompts" /> */}
